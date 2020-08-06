ed51d12f22b7f0c525f0bd6cf938c4cd49056d1c
Shivam Rathore <shivam.rathore010@gmail.com>
2020-08-07 01:44:11 +0530

    Add new ball with motion accelaration

---
+++ Assets/Scripts/Accelerate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerate : MonoBehaviour
+{
+    private Rigidbody2D com;
+
+    void Start()
+    {
+        com = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        com.AddForce(Input.acceleration);
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        com.AddForce(Vector2.MoveTowards(this.transform.position, worldPosition, 8 * Time.deltaTime));
+        //this.transform.position = Vector2.MoveTowards(this.transform.position, worldPosition, 8*Time.deltaTime);
+    }
+
+
+}
