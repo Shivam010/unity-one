b23f4cf82cb8b7ee77a36b92c371bf48fde66f3b
Shivam Rathore <shivam.rathore010@gmail.com>
2020-08-06 16:26:37 +0530

    initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-2, 2);
+        float y = Random.Range(1, 4);
+        if (gameObject.name == "Ball (3)" || gameObject.name == "Ball (2)")
+        {
+            y = Random.Range(-1, 2);
+        }
+        gameObject.transform.position = new Vector2(x, y);
+    }
+    private void OnMouseDown()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().ScoreUp();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject winText;
+    public GameObject button, play, balls;
+    int score = 0;
+
+    public void ScoreUp() {
+        score++;
+        if (score == 4 ){
+            Win();
+        }
+    }
+
+    public void Win () {
+        winText.SetActive(true);
+        button.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game"); 
+    }
+
+    public void Play()
+    {
+        balls.SetActive(true);
+        play.SetActive(false);
+    }
+}
